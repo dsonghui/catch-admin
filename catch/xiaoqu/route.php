386b2575b339c1a8c1990abdc8d7e4dfb091e1ec
david <david.deng@jcinfotech.com>
2020-06-03 23:01:24 +0800

    小区列表

--- catch/xiaoqu/route.php
+++ catch/xiaoqu/route.php
@@ -11,3 +11,4 @@
 
 $router->resource('xiaoqu', '\catchAdmin\xiaoqu\controller\XiaoQu');
 $router->resource('dianti', '\catchAdmin\xiaoqu\controller\DianTi');
+$router->get('istest', '\catchAdmin\xiaoqu\controller\IsTest@index');
--- database/migrations/20200517093224_xiao_qu.php
+++ database/migrations/20200517093224_xiao_qu.php
@@ -28,15 +28,17 @@ class XiaoQu extends Migrator
      */
     public function change()
     {
-        $table  =  $this->table('xiaoqu',array('engine'=>'Innodb', 'comment' => '小区列表', 'signed' => false));
-        $table->addColumn('name', 'string',array('limit'  =>  15,'default'=>'','comment'=>'小区名称'))
-            ->addColumn('shou_zi_mu', 'string',array('limit'  =>  2, 'comment'=>'首字母'))
-            ->addColumn('bian_hao', 'string',array('limit'  =>  100, 'comment'=>'小区编号'))
-            ->addColumn('creator_id', 'integer',['default' => 0, 'comment'=>'创建人ID'])
-            ->addColumn('status', 'boolean',array('limit'  =>  1,'default'=> 1,'comment'=>'用户状态 1 正常 2 禁用'))
-            ->addColumn('created_at', 'integer', array('default'=>0,'comment'=>'创建时间', 'signed' => false ))
-            ->addColumn('updated_at', 'integer', array('default'=>0,'comment'=>'更新时间', 'signed' => false))
-            ->addColumn('deleted_at', 'integer', array('default'=>0,'comment'=>'删除状态，0未删除 >0 已删除', 'signed' => false))
+        $table = $this->table('xiaoqu', array('engine' => 'Innodb', 'comment' => '小区列表', 'signed' => false));
+        $table->addColumn('name', 'string', array('limit' => 15, 'default' => '', 'comment' => '小区名称'))
+            ->addColumn('shou_zi_mu', 'string', array('limit' => 2, 'default' => '', 'comment' => '首字母'))
+            ->addColumn('bian_hao', 'string', array('limit' => 100, 'default' => '', 'comment' => '小区编号'))
+            ->addColumn('address', 'string', ['limit' => 200, 'default' => '', 'comment' => '小区地址'])
+            ->addColumn('creator_id', 'integer', ['default' => 0, 'comment' => '创建人ID'])
+            ->addColumn('status', 'boolean', array('limit' => 1, 'default' => 1, 'comment' => '用户状态 1 正常 2 禁用'))
+            ->addColumn('sort', 'integer', array('default' => 0, 'comment' => '排序'))
+            ->addColumn('created_at', 'integer', array('default' => 0, 'comment' => '创建时间', 'signed' => false))
+            ->addColumn('updated_at', 'integer', array('default' => 0, 'comment' => '更新时间', 'signed' => false))
+            ->addColumn('deleted_at', 'integer', array('default' => 0, 'comment' => '删除状态，0未删除 >0 已删除', 'signed' => false))
             ->create();
     }
 }
